4730c839107e8959d814d8c5c5d74a46d1f38d3e
berniluciano <luciano.berni@airbits.com.ar>
2020-07-25 15:41:59 -0300

    docs agregado

---
+++ docs/precache-manifest.ee05917a9c415dd5049fef48617f280b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852bf2b2c81eb6a81d089e79f536b7d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3884104816ea45b8841a",
+    "url": "/static/css/main.a4b0ed90.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3884104816ea45b8841a",
+    "url": "/static/js/main.bcebdb43.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
